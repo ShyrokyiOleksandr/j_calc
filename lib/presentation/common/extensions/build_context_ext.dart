6533d16e95bd39eee0ce12a0edeb11ce6a1bc797
Oleksandr Shyrokyi <oleksandr.shyrokyi@otakoyi.com>
2025-06-10 19:23:19 +0200

    replace l10n folder to presentation/common

--- lib/presentation/common/extensions/build_context_ext.dart
+++ lib/presentation/common/extensions/build_context_ext.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:j_calc/l10n/app_localizations.dart';
+import 'package:j_calc/presentation/common/l10n/app_localizations.dart';
 
 extension BuildContextExt on BuildContext {
   AppLocalizations get l10n => AppLocalizations.of(this)!;
